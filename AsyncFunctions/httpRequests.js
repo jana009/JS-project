9e35071d1864862e7d78b441359f2176b12a9f59
janani09 <jananimurthi09@gmail.com>
2022-08-04 01:55:27 +0530

    Added the functionality for callback

--- AsyncFunctions/httpRequests.js
+++ AsyncFunctions/httpRequests.js
@@ -1,13 +1,38 @@
-const request=new XMLHttpRequest();
-const result=document.getElementById('result');
-console.log(result.innerText);
+const getTodos=(resource,callback)=>{
+    const request=new XMLHttpRequest();
+    const result=document.getElementById('result');
+    console.log(result.innerText);
 
-request.addEventListener('readystatechange',()=>{
-    console.log(request,request.readyState);
-    if(request.readyState==4){
-        result.innerText=request.response;
+    request.addEventListener('readystatechange',()=>{
+        if(request.readyState===4 && request.status===200){
+            callback(undefined,'Whoa! Success')
+            console.log(JSON.parse(request.responseText));
+            result.innerText=request.response;
+        }
+        else if(request.readyState===4){
+            callback('OOPS :( No result',undefined);
+        }
+    });
+
+    request.open("GET",resource);
+    request.send();
+};
+
+getTodos('todos/priorityHigh.json',(err,data)=>{
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log(data);
+        getTodos('todos/priorityModerate.json',(err,data)=>{
+            if(data){
+                console.log(data)
+                getTodos('todos/priorityLow.json',(err,data)=>{
+                    if(data){
+                        console.log(data);
+                    }
+                })
+            }
+        })
     }
 });
-
-request.open("GET","https://jsonplaceholder.typicode.com/todos/1");
-request.send();
