38d9913f6a49cd0667d0b46306133bcbc623c4db
janani09 <jananimurthi09@gmail.com>
2022-08-15 15:55:28 +0530

    Weather Application

---
+++ weather-app/scripts/app.js
@@ -0,0 +1,52 @@
+const cityField= document.querySelector('form');
+const card= document.querySelector('.card');
+const details= document.querySelector('.details');
+const time=document.querySelector('img.time');
+const icon=document.querySelector('.icon img');
+const updateUI=(data)=>{
+    const {cityDet,weatherDet}=data;
+    console.log(cityDet);
+    console.log(weatherDet);
+    details.innerHTML=`
+    <h5 class="my-3">${cityDet.EnglishName}</h5>
+    <div class="my-3">${weatherDet.WeatherText}</div>
+    <div class="display-4 my-4">
+        <span>${weatherDet.Temperature.Metric.Value}</span>
+        <span>&deg;C</span>
+    </div>`;
+    
+    let iconSrc=null;
+    iconSrc=`img/icons/${weatherDet.WeatherIcon}.svg`;
+    icon.setAttribute('src',iconSrc);
+
+    let timeSrc=null;
+    console.log(weatherDet.IsDayTime);
+    timeSrc=weatherDet.IsDayTime ? 'img/day.svg' : 'img/night.svg';
+    console.log(timeSrc);
+    time.setAttribute('src',timeSrc); 
+
+    if(card.classList.contains('d-none')){
+        card.classList.remove('d-none');
+    }
+
+}
+
+
+
+const updateCity=async(cityName)=>{
+    const cityDet= await getCity(cityName);
+    const weatherDet=await getWeather(cityDet.Key);
+    console.log(cityDet,weatherDet);
+    return {cityDet,weatherDet};
+}
+
+
+cityField.addEventListener('submit',e=>{
+    e.preventDefault();
+    const cityName= cityField.city.value;
+     cityField.reset();
+    updateCity(cityName)
+    .then(data=>updateUI(data))
+    .catch(err=>console.error(err));
+    
+});
--- weather-app/scripts/forecast.js
+++ weather-app/scripts/forecast.js
@@ -1,16 +1,30 @@
 const key="IlAdLD6QiWGcVlhpKGve6F6g7tq6AkeT";
-const baseUrl="http://dataservice.accuweather.com/locations/v1/cities/search";
-const queryParams=`?apikey=${key}&q=tiruvallur`;
-const getCity =async()=>{
+
+
+const getWeather=async(id)=>{
+    const baseUrl="http://dataservice.accuweather.com/currentconditions/v1/";
+    const queryParams=`${id}?apikey=${key}`;
     const response = await fetch(baseUrl+queryParams);
     if(response.status!==200){
         return new Error('Error while fetching the URL')
     }
-    const data = response.json();
-    return data;
+    const data = await response.json();
+    return data[0];
 }
-getCity().then((data)=>{
-const city=data[0].Key;
-console.log(city);
 
-}) .catch((err)=>console.log(err));
+//city information
+const getCity =async(city)=>{
+    const baseUrl="http://dataservice.accuweather.com/locations/v1/cities/search";
+    const queryParams=`?apikey=${key}&q=${city}`;
+    const response = await fetch(baseUrl+queryParams);
+    if(response.status!==200){
+        return new Error('Error while fetching the URL')
+    }
+    const data = await response.json();
+    return data[0];
+}
+/* getCity(cityName).then((data)=>{
+    const city=data.Key;
+    return getWeather(city);
+}).then(data=>{return data})
+  .catch((err)=>console.log(err)); */
